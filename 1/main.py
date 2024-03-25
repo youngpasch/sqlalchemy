0dbbd09b0e049df2a9d0d58c6080997d833091ae
Lesha <qu4rtz43@gmail.com>
2024-03-25 20:47:44 +0300

    add three colonists and a captain

--- 1/main.py
+++ 1/main.py
@@ -24,15 +24,39 @@ def main():
     db_sess.commit()
 
     colonist1 = User()
-    colonist1.surname = "Scott"
-    colonist1.name = "Ridley"
-    colonist1.age = "21"
-    colonist1.position = "captain"
-    colonist1.speciality = "research engineer"
+    colonist1.surname = "A"
+    colonist1.name = "A"
+    colonist1.age = "1"
+    colonist1.position = "colonist"
+    colonist1.speciality = "pilot"
     colonist1.address = "module_1"
-    colonist1.email = "scott_chief@mars.org"
+    colonist1.email = "AAAAAA@mars.org"
     db_sess = db_session.create_session()
     db_sess.add(colonist1)
+    db_sess.commit()
+
+    colonist2 = User()
+    colonist2.surname = "B"
+    colonist2.name = "B"
+    colonist2.age = "2"
+    colonist2.position = "colonist"
+    colonist2.speciality = "doctor"
+    colonist2.address = "module_1"
+    colonist2.email = "BBBBBBB@mars.org"
+    db_sess = db_session.create_session()
+    db_sess.add(colonist2)
+    db_sess.commit()
+
+    colonist3 = User()
+    colonist3.surname = "C"
+    colonist3.name = "C"
+    colonist3.age = "3"
+    colonist3.position = "colonist"
+    colonist3.speciality = "scientist"
+    colonist3.address = "module_1"
+    colonist3.email = "CCCCCC@mars.org"
+    db_sess = db_session.create_session()
+    db_sess.add(colonist3)
     db_sess.commit()
 
     # app.run()
